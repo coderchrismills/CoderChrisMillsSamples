3a2de52e00f64aa66ef7045c8d903b9f55a41f43
Chris Mills <coderchrismills@gmail.com>
2013-08-25 10:41:31 -0700

    Revert "Integrated new BNHtmlPdfKit"
    This reverts commit 60d69fd01fe3900d358bd8067d4ed8be01aa8d09.

--- PrintTest2/PrintTest2/Source/WebPopOverViewController.m
+++ PrintTest2/PrintTest2/Source/WebPopOverViewController.m
@@ -52,7 +52,8 @@
     
     // Generate the pdf. 
     [htmlPdfKit saveHtmlAsPdf:htmlString
-                       toFile:[self pdfFilePath]];
+                       toFile:[self pdfFilePath]
+                       inView:v];
 
 }
 /****************************************************************************/
